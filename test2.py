94d08204d1d86cb1bb025678f18383ce44cbdaac
indextrown <69367698+indextrown@users.noreply.github.com>
2021-09-11 18:07:52 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -12,3 +12,11 @@ def test_driver():
     driver.quit()
 
     assert driver is not None
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+wd = driver = webdriver.Chrome(chrome_driver, options=chrome_options)
+wd.get("http://www.naver.com")
+wd.get_screenshot_as_file("TEST.png")
