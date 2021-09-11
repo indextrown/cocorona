16b159bd447b7aa41bdbae0bc344cd39694ac650
indextrown <69367698+indextrown@users.noreply.github.com>
2021-09-11 18:02:53 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -4,7 +4,17 @@ from time import sleep
 url = "http://www.naver.com" 
 
 # chromedriver 
-driver = webdriver.Chrome("https://github.com/indextrown/testt/blob/main/chromedriver")
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')               # headless
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(chrome_driver, options=chrome_options)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')               # headless
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--disable-gpu')
 driver.get(url)
 
 # adding the loading time.
