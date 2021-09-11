871d0bed277e84ca4e51c59d79ac7dbe8d322b3a
indextrown <69367698+indextrown@users.noreply.github.com>
2021-09-11 18:05:49 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,30 +1,14 @@
+import os
 from selenium import webdriver
-from time import sleep
-
-url = "http://www.naver.com" 
-
-# chromedriver 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')               # headless
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(chrome_driver, options=chrome_options)
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')               # headless
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--disable-gpu')
-driver.get(url)
-
-# adding the loading time.
-sleep(0.1)
-
-# chapture!
-driver.save_screenshot("screenshot.png")
-
-# 브라우저를 닫는다.
-driver.close()
 
 
+def test_driver():
+    chrome_driver = os.path.join('chromedriver')
 
+    chrome_options = webdriver.ChromeOptions()
+
+    driver = webdriver.Chrome(chrome_driver, options=chrome_options)
+    driver.implicitly_wait(3)
+    driver.quit()
+
+    assert driver is not None
