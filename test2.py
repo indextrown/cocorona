57335e36d0db0c850d6ad5d8f9b995f1f956fb2a
indextrown <69367698+indextrown@users.noreply.github.com>
2021-09-11 18:40:19 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -10,7 +10,7 @@ chrome_driver = os.path.join('chromedriver')
 driver = webdriver.Chrome(chrome_driver, options=chrome_options)
 driver.get("http://ncov.mohw.go.kr/bdBoardList_Real.do")
 driver.get_screenshot_as_file("TEST1.png")
-element = driver.find_element_by_class_name("c_chart c_chart_pt")
+element = driver.find_element_by_class_name("c_chart.c_chart_pt")
 element_png = element.screenshot_as_png
 with open('ttt.png', "wb") as file:
   file.write(element_png)
