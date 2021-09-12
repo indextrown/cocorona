a15352b52693e1646e7c0f5652c8f3b9968f1c07
indextrown <69367698+indextrown@users.noreply.github.com>
2021-09-12 12:10:24 +0900

    Update test3.py

--- test3.py
+++ test3.py
@@ -10,7 +10,7 @@ chrome_driver = os.path.join('chromedriver')
 driver = webdriver.Chrome(chrome_driver, options=chrome_options)
 driver.get("http://ncov.mohw.go.kr/")
 driver.get_screenshot_as_file("TEST2.png")
-element = driver.find_element_by_class_name("rssm_graph")
+element = driver.find_element_by_class_name("rss_map")
 element_png = element.screenshot_as_png
 with open('TEST2.png', "wb") as file:
   file.write(element_png)
