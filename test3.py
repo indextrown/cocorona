6f813adeb5597239a567b0ef8221d661341630c8
indextrown <69367698+indextrown@users.noreply.github.com>
2021-09-12 12:05:10 +0900

    Update test3.py

--- test3.py
+++ test3.py
@@ -12,7 +12,7 @@ driver.get("http://ncov.mohw.go.kr/")
 driver.get_screenshot_as_file("TEST2.png")
 element = driver.find_element_by_class_name("rssm_graph")
 element_png = element.screenshot_as_png
-with open('ttt.png', "wb") as file:
+with open('TEST2.png', "wb") as file:
   file.write(element_png)
 
 driver.quit()
