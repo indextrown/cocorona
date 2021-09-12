c1f77dc0abffb314c742635f8524b48b653fc34d
indextrown <69367698+indextrown@users.noreply.github.com>
2021-09-12 12:02:19 +0900

    Create test3.py

---
+++ test3.py
@@ -0,0 +1,18 @@
+import os
+from selenium import webdriver
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_driver = os.path.join('chromedriver')
+
+driver = webdriver.Chrome(chrome_driver, options=chrome_options)
+driver.get("http://ncov.mohw.go.kr/bdBoardList_Real.do")
+driver.get_screenshot_as_file("TEST1.png")
+element = driver.find_element_by_class_name("c_chart.c_chart_pt")
+element_png = element.screenshot_as_png
+with open('ttt.png', "wb") as file:
+  file.write(element_png)
+
+driver.quit()
