68f65db642cfad415e36cd8ea5029a836249b0ce
indextrown <69367698+indextrown@users.noreply.github.com>
2021-09-12 12:35:45 +0900

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,1 +1,18 @@
+import os
+from selenium import webdriver
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_driver = os.path.join('chromedriver')
+
+driver = webdriver.Chrome(chrome_driver, options=chrome_options)
+driver.get("http://ncov.mohw.go.kr/")
+driver.get_screenshot_as_file("TEST4.png")
+element = driver.find_element_by_class_name("t_dp_n m_dp_n")
+element_png = element.screenshot_as_png
+with open('TEST4.png', "wb") as file:
+  file.write(element_png)
+
+driver.quit()
