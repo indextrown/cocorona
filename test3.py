0885e0a8d474b05d918ce9d358582ad25e0d185e
indextrown <69367698+indextrown@users.noreply.github.com>
2021-09-12 12:03:59 +0900

    Update test3.py

--- test3.py
+++ test3.py
@@ -8,9 +8,9 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_driver = os.path.join('chromedriver')
 
 driver = webdriver.Chrome(chrome_driver, options=chrome_options)
-driver.get("http://ncov.mohw.go.kr/bdBoardList_Real.do")
-driver.get_screenshot_as_file("TEST1.png")
-element = driver.find_element_by_class_name("c_chart.c_chart_pt")
+driver.get("http://ncov.mohw.go.kr/")
+driver.get_screenshot_as_file("TEST2.png")
+element = driver.find_element_by_class_name("rssm_graph")
 element_png = element.screenshot_as_png
 with open('ttt.png', "wb") as file:
   file.write(element_png)
