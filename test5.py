56efadcdd3d9910819aecb49872f2c4b123f12c7
indextrown <69367698+indextrown@users.noreply.github.com>
2021-09-13 22:21:52 +0900

    Update test5.py

--- test5.py
+++ test5.py
@@ -10,7 +10,7 @@ chrome_driver = os.path.join('chromedriver')
 driver = webdriver.Chrome(chrome_driver, options=chrome_options)
 driver.get("http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=13&ncvContSeq=&contSeq=&board_id=&gubun=")
 driver.get_screenshot_as_file("TEST5.png")
-element = driver.find_element_by_id("ncov_tab_content.on")
+element = driver.find_element_by_class_name("ncov_tab_content.on")
 element_png = element.screenshot_as_png
 with open('TEST5.png', "wb") as file:
   file.write(element_png)
