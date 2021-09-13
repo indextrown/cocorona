32176046c1fd42ca600e18bce1a05c0546430eb4
indextrown <69367698+indextrown@users.noreply.github.com>
2021-09-13 22:23:19 +0900

    Update test5.py

--- test5.py
+++ test5.py
@@ -8,7 +8,7 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_driver = os.path.join('chromedriver')
 
 driver = webdriver.Chrome(chrome_driver, options=chrome_options)
-driver.get("http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=13&ncvContSeq=&contSeq=&board_id=&gubun=")
+driver.get("http://ncov.mohw.go.kr/")
 driver.get_screenshot_as_file("TEST5.png")
 element = driver.find_element_by_class_name("ncov_tab_content.on")
 element_png = element.screenshot_as_png
