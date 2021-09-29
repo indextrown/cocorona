178862a7bf8f11520f89e8c3dece4a3f39d82089
indextrown <69367698+indextrown@users.noreply.github.com>
2021-09-29 11:53:31 +0900

    Create test6.py

---
+++ test6.py
@@ -0,0 +1,18 @@
+import os
+from selenium import webdriver
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_driver = os.path.join('chromedriver')
+
+driver = webdriver.Chrome(chrome_driver, options=chrome_options)
+driver.get("https://www.jinju.go.kr/05190/05641.web")
+driver.get_screenshot_as_file("TEST6.png")
+element = driver.find_element_by_class_name("pr1")
+element_png = element.screenshot_as_png
+with open('TEST5.png', "wb") as file:
+  file.write(element_png)
+
+driver.quit()
