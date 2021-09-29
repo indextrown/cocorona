9802856f547bd61a314200ff5680e4067a941e5a
indextrown <69367698+indextrown@users.noreply.github.com>
2021-09-29 12:01:30 +0900

    Update test6.py

--- test6.py
+++ test6.py
@@ -9,10 +9,12 @@ chrome_driver = os.path.join('chromedriver')
 
 driver = webdriver.Chrome(chrome_driver, options=chrome_options)
 driver.get("https://www.jinju.go.kr/05190/05641.web")
+body.find_element_by_class_name("btn_more").click()
+
 driver.get_screenshot_as_file("TEST6.png")
 element = driver.find_element_by_class_name("pr1")
 element_png = element.screenshot_as_png
-with open('TEST5.png', "wb") as file:
+with open('TEST6.png', "wb") as file:
   file.write(element_png)
 
 driver.quit()
