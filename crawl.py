a80070c39c8c1e476b60dd3fb6447587842e6eaf
indextrown <69367698+indextrown@users.noreply.github.com>
2021-11-10 12:13:57 +0900

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -8,9 +8,9 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_driver = os.path.join('chromedriver')
 
 driver = webdriver.Chrome(chrome_driver, options=chrome_options)
-driver.get("http://ncov.mohw.go.kr/bdBoardList_Real.do")
+driver.get("http://ncov.mohw.go.kr/")
 driver.get_screenshot_as_file("TEST20.png")
-element = driver.find_element_by_class_name("c_chart.c_chart_pt")
+element = driver.find_element_by_class_name("weeklyWrap")
 element_png = element.screenshot_as_png
 with open('ttt.png', "wb") as file:
   file.write(element_png)
