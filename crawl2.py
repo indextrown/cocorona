256f300491253a44b49ce08dfd8a8612ed1d8403
indextrown <69367698+indextrown@users.noreply.github.com>
2021-11-10 12:38:13 +0900

    Update crawl2.py

--- crawl2.py
+++ crawl2.py
@@ -1,1 +1,18 @@
+import os
+from selenium import webdriver
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_driver = os.path.join('chromedriver')
+
+driver = webdriver.Chrome(chrome_driver, options=chrome_options)
+driver.get("http://ncov.mohw.go.kr/")
+driver.get_screenshot_as_file("TEST21.png")
+element = driver.find_element_by_class_name("live_left")
+element_png = element.screenshot_as_png
+with open('TEST21.png', "wb") as file:
+  file.write(element_png)
+
+driver.quit()
